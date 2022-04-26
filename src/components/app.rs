474f95dd90f1d4c63f6fee19977185bb66ede2c2
Branco <branco.bruyneel@gmail.com>
2022-04-26 11:31:00 +0200

    feat: tasks

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,8 +1,59 @@
 use yew::prelude::*;
 
+#[derive(Clone, PartialEq)]
+struct Task {
+    id: usize,
+    title: String,
+    completed: bool,
+}
+
+#[derive(Properties, PartialEq)]
+struct TaskListProps {
+    tasks: Vec<Task>,
+}
+
+#[function_component]
+fn TaskList(TaskListProps { tasks }: &TaskListProps) -> Html {
+    tasks
+        .iter()
+        .map(|task| {
+            let task_id = format!("task-{}", task.id);
+            html! {
+                <div class="flex gap-2 justify-center items-center">
+                    <input type="checkbox" id={task_id.clone()} checked={task.completed} />
+                    <label for={task_id.clone()}>{task.title.clone()}</label>
+                </div>
+            }
+        })
+        .collect()
+}
+
 #[function_component]
 pub fn App() -> Html {
+    let tasks = vec![
+        Task {
+            id: 1,
+            title: "Take dog out for a walk".to_string(),
+            completed: true,
+        },
+        Task {
+            id: 2,
+            title: "Feed the cats".to_string(),
+            completed: false,
+        },
+        Task {
+            id: 3,
+            title: "Water plants".to_string(),
+            completed: false,
+        },
+    ];
+
     html! {
-        <h1>{ "Hello World" }</h1>
+        <div class="container-sm mx-auto text-center">
+            <h1 class="font-bold text-4xl my-4">{ "My todo list" }</h1>
+            <div class="flex flex-col">
+                <TaskList tasks={tasks}  />
+            </div>
+        </div>
     }
 }
