6b847e589ac541275e6b989a5df1c953290ae2df
Branco <branco.bruyneel@gmail.com>
2022-04-29 09:40:29 +0200

    fix(api): rename localhost -> 127.0.0.1

--- api/src/main.rs
+++ api/src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(
                 Cors::default()
-                    .allowed_origin("http://localhost:8080")
+                    .allowed_origin("http://127.0.0.1:8080")
                     .allowed_methods(vec!["GET", "POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
